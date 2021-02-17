8d9866b42a7faa994506d19532e50d569743a797
Julius <julius@MacBook-Air-von-Julius.fritz.box>
2021-02-17 15:13:10 +0100

    Weitere Grundstücke hinzugefügt

---
+++ Programm/src/new_root/Grundstuecke/Bahnhof.py
@@ -0,0 +1,21 @@
+﻿from .Grundstueck import Grundstueck
+
+class Bahnhof(Grundstueck):
+
+    def __init__(self, name, feld):
+        self.name = name
+        self.feld = feld
+        self.grundstueckWert = 200
+        self.hypothekWert = 100
+
+    def mieteBerechnen(self, anzahlBahnhofe):
+        if anzahlBahnhofe == 1:
+            return 25
+        elif anzahlBahnhofe == 2:
+            return 50
+        elif anzahlBahnhofe == 3:
+            return 100
+        elif anzahlBahnhofe == 4:
+            return 200
+        else:
+            return 0
--- Programm/src/new_root/Grundstuecke/Grundbuch.py
+++ Programm/src/new_root/Grundstuecke/Grundbuch.py
@@ -1,4 +1,5 @@
 ﻿from .Straße import Strasse
+from .Bahnhof import Bahnhof
 from ..System.Felder import Felder
 from .Farbe import Farben
 from .Farbgruppe import Farbgruppe
@@ -6,12 +7,16 @@ from .Farbgruppe import Farbgruppe
 # Liste aller Grundstücke
 alleGrundstuecke = [
     Strasse(Felder.Badstrasse.name, Felder.Badstrasse, 60, 30, Farben.Braun, 2, 10, 30, 90, 160, 250, 50),
-    Strasse(Felder.Turmstrasse.name, Felder.Turmstrasse, 60, 30, Farben.Braun, 4, 20, 60, 180, 320, 450, 50)
-
+    Strasse(Felder.Turmstrasse.name, Felder.Turmstrasse, 60, 30, Farben.Braun, 4, 20, 60, 180, 320, 450, 50),
+    Bahnhof(Felder.Suedbahnhof.name, Felder.Suedbahnhof),
+    Strasse(Felder.Chaussestrasse.name, Felder.Chaussestrasse, 100, 50, Farben.Blau, 6, 30, 90, 270, 400, 550, 50),
+    Strasse(Felder.Elisenstrasse.name, Felder.Elisenstrasse, 100, 50, Farben.Blau, 6, 30, 90, 270, 400, 550, 50),
+    Strasse(Felder.Poststrasse.name, Felder.Poststrasse, 120, 60, Farben.Blau, 8, 40, 100, 300, 450, 600, 50)
 ]
 
 alleFarbgruppen = {
-    Farbgruppe(Farben.Braun, [alleGrundstuecke[0], alleGrundstuecke[1]])
+    Farbgruppe(Farben.Braun, [alleGrundstuecke[0], alleGrundstuecke[1]]),
+    Farbgruppe(Farben.Blau, [alleGrundstuecke[3], alleGrundstuecke[4], alleGrundstuecke[5]])
 }
 
 
